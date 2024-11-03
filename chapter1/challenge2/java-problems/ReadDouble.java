f3de8010c123f4f71a9d25d322985f37d2423dad
gersonvidal <gtotrac@gmail.com>
2024-11-03 15:14:57 -0600

    finished Read Double exercise

--- chapter1/challenge2/java-problems/ReadDouble.java
+++ chapter1/challenge2/java-problems/ReadDouble.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
-
 public class ReadDouble {
-
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		double n = sc.nextDouble();
-		S
-	}	
+		double n = new Scanner(System.in).nextDouble();
+		System.out.printf("%7.3f", n);
+	}
 }
